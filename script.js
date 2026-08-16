dabdda8fc4553c42279b549f746256d7a55bc283
Baskoro Muhammad Mutakin <baskorob340@gmail.com>
2026-08-16 08:00:30 +0000

    Create portfolio website

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    // Smooth navigation
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener("click", function (event) {
+
+            const targetId = this.getAttribute("href");
+
+            if (targetId === "#") {
+                return;
+            }
+
+            const target = document.querySelector(targetId);
+
+            if (target) {
+                event.preventDefault();
+
+                target.scrollIntoView({
+                    behavior: "smooth"
+                });
+            }
+
+        });
+    });
+
+});
